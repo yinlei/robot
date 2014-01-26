fed89c4a39197e30eceafe6cc9665390ec1b1812
yinl <yinlei.zhi@gmail.com>
2014-01-26 15:41:18 +0800

    Update intall.bat

--- install.bat
+++ install.bat
@@ -16,6 +16,11 @@ gofmt -w src
 
 go get code.google.com/p/goprotobuf/proto
 go get code.google.com/p/goprotobuf/protoc-gen-go
+go get github.com/go-sql-driver/mysql
+go get github.com/codegangsta/martini
+go get github.com/golang/groupcache
+go get github.com/goraft/raft
+go get code.google.com/p/log4go
 
 :end
 echo finished
